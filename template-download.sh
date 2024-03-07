c54ef13ce865bd98b1babb6b87e3f9d71741f58c
“Brian <bcurnow@users.noreply.github.com>
2024-03-07 15:20:46 -0500

    Actually fixed for loop and GitHub location

--- template-download.sh
+++ template-download.sh
@@ -13,9 +13,10 @@ echo "Ensuring /opt/template-config exists"
 mkdir -p /opt/template-config
 
 echo "Downloading lastest version of the template-config scripts"
-for script in "make-template.sh config-template.sh template-download.sh"
+for script in make-template.sh config-template.sh template-download.sh
 do
-curl --silent -o /opt/template-config/${script} --location https://github.com/bcurnow/raw/main/${script}
+echo "Downloading ${script}"
+curl --silent -o /opt/template-config/${script} --location https://github.com/bcurnow/template-config/raw/main/${script}
 if [ $? -ne 0 ]
 then
   echo "Download of ${script} failed" >&2
