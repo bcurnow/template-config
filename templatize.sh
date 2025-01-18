f78b25eda7345fac6d251ad563935113a6869dc7
bcurnow <bcurnow@users.noreply.github.com>
2025-01-18 00:31:54 -0500

    Make the user a variable to make changing the non-root user easier

--- templatize.sh
+++ templatize.sh
@@ -2,8 +2,10 @@
 #
 # Reconfigures the VM to be a proxmox template
 #
+defaultUser=bcurnow
+
 cat <<EOF
-Please ensure the following commands are run as the non-root user before executing:
+Please ensure the following commands are run as ${defaultUser} before executing:
 
   set +o history # Turns off history for the current shell
   history -c     # Clears current history
@@ -48,17 +50,17 @@ echo "Generating a new SSH keys"
 sudo rm -f /etc/ssh/ssh_host_*
 sudo ssh-keygen -A
 
-echo "Clearing bcurnow user configuration and history"
-rm -rf /home/bcurnow/.ssh/
-rm -f /home/bcurnow/anaconda-ks.cfg
-rm -f /home/bcurnow/bash_history
-rm -f /home/bcurnow/lesshst
-rm -rf /home/bcurnow/local
+echo "Clearing ${defaultUser} user configuration and history"
+rm -rf /home/${defaultUser}/.ssh/
+rm -f /home/${defaultUser}/anaconda-ks.cfg
+rm -f /home/${defaultUser}/bash_history
+rm -f /home/${defaultUser}/lesshst
+rm -rf /home/${defaultUser}/local
 
 # Setup authorized key login for my public key
-mkdir -p /home/bcurnow/ssh
-chmod 700 /home/bcurnow/ssh
-cat <<EOF >/home/bcurnow/ssh/authorized_keys
+mkdir -p /home/${defaultUser}/ssh
+chmod 700 /home/${defaultUser}/ssh
+cat <<EOF >/home/${defaultUser}/ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJMQ7gGnSRqhGbeiFm3MAfJ2DzJc3UMPBazhZIoXYLbKXaUFKPV2YuvTDeaXEa1UiAoxQJmhq94ABc2kPBfdfPSVd0elOKiKBbdpwO5PrKxK3DpxdX46GgKp0kRW8a3UgAUOuo0nigaEd7pWlkJ8+zxR0aFzfpbRiqIHTT8L3gVsRiQIrs0vkwn7sUMQs7ODJGz2bBuL6aI5aPyiyxoMlLfeo7AabnBIXCM5Bfym6m0/KmUkSugWyOgKXMCscBNiclC3QO/ExjouKnrlXQg9f/+I2J3FAex/QRRl1m7G1NPYygd1NIVcoNCIrU4g5aZkKqCk0DZC08mKVZ2zuRtqaluGMEfYd6LMGXSjuaFYDmtybvwEgvSlT9fkDCZcwF65YBnHXdr/QNWG4D5U3tXh5o4H202o6rsdsVhIsKIAkFqiiiC3yeCWiDVR2wQNENNkMbL/7tZMSqRm31iJjvQNuCBPpu6Z59DNkmZqb8dDgrOyi8SREBKf7FLuKx/jp7R4k= Brian.Curnow@T07M6PT2TT
 EOF
 
