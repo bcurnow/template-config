3a5cfc13e439b3c76aaff139306e9b54ceac9067
bcurnow <bcurnow@users.noreply.github.com>
2025-01-18 00:28:54 -0500

    Added missing sudo to the ssh-keygen, remove root check as ensuring that the non-root user clears the history should be enough to clear everything out

--- templatize.sh
+++ templatize.sh
@@ -2,16 +2,14 @@
 #
 # Reconfigures the VM to be a proxmox template
 #
-if [ ${EUID} -eq 0 ]
-then
-  echo "This script must not be run as root" >&2
-  exit 1
-fi
+cat <<EOF
+Please ensure the following commands are run as the non-root user before executing:
 
-echo "Please ensure the following commands are run as the non-root user before executing:"
-echo "  set +o history # Turns off history for the current shell"
-echo "  history -c     # Clears current history"
-echo "This will ensure there's no extraneous command history when the template is cloned"
+  set +o history # Turns off history for the current shell
+  history -c     # Clears current history
+
+This will ensure there's no extraneous command history when the template is cloned
+EOF
 read -p "Press any key to continue..." -n 1 -r
 
 echo "Ensuring hostname is 'debian-template'"
@@ -48,19 +46,19 @@ EOF
 
 echo "Generating a new SSH keys"
 sudo rm -f /etc/ssh/ssh_host_*
-ssh-keygen -A
+sudo ssh-keygen -A
 
-echo "Clearing current user configuration and history"
-rm -rf ~/.ssh/
-rm -f ~/anaconda-ks.cfg
-rm -f ~/.bash_history
-rm -f ~/.lesshst
-rm -rf ~/.local
+echo "Clearing bcurnow user configuration and history"
+rm -rf /home/bcurnow/.ssh/
+rm -f /home/bcurnow/anaconda-ks.cfg
+rm -f /home/bcurnow/bash_history
+rm -f /home/bcurnow/lesshst
+rm -rf /home/bcurnow/local
 
 # Setup authorized key login for my public key
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-cat <<EOF >~/.ssh/authorized_keys
+mkdir -p /home/bcurnow/ssh
+chmod 700 /home/bcurnow/ssh
+cat <<EOF >/home/bcurnow/ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJMQ7gGnSRqhGbeiFm3MAfJ2DzJc3UMPBazhZIoXYLbKXaUFKPV2YuvTDeaXEa1UiAoxQJmhq94ABc2kPBfdfPSVd0elOKiKBbdpwO5PrKxK3DpxdX46GgKp0kRW8a3UgAUOuo0nigaEd7pWlkJ8+zxR0aFzfpbRiqIHTT8L3gVsRiQIrs0vkwn7sUMQs7ODJGz2bBuL6aI5aPyiyxoMlLfeo7AabnBIXCM5Bfym6m0/KmUkSugWyOgKXMCscBNiclC3QO/ExjouKnrlXQg9f/+I2J3FAex/QRRl1m7G1NPYygd1NIVcoNCIrU4g5aZkKqCk0DZC08mKVZ2zuRtqaluGMEfYd6LMGXSjuaFYDmtybvwEgvSlT9fkDCZcwF65YBnHXdr/QNWG4D5U3tXh5o4H202o6rsdsVhIsKIAkFqiiiC3yeCWiDVR2wQNENNkMbL/7tZMSqRm31iJjvQNuCBPpu6Z59DNkmZqb8dDgrOyi8SREBKf7FLuKx/jp7R4k= Brian.Curnow@T07M6PT2TT
 EOF
 
