2777299a4434a23432d725039c773f9014a130ec
bcurnow <mailto:bcurnow@users.noreply.github.com>
2026-08-08 02:51:45 -0400

    Updated to really make sure it matches what debian-hardened will do

--- templatize.sh
+++ templatize.sh
@@ -56,7 +56,7 @@ echo "debian-template" | sudo tee /etc/hostname >/dev/null
 
 echo "Updating /etc/hosts"
 sudo tee /etc/hosts >/dev/null << EOF
-127.0.0.1	debian-template.${domain} debian-template localhost # managed by ansible
+127.0.0.1 debian-template.${domain} debian-template localhost # managed by ansible
 EOF
 
 echo "Clearing the machine id"
